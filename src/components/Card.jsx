0eb00a22e26ecef2fd365cb5d31a46aead28d3fb
Shishir <kushwahashishir1112@gmail.com>
2023-12-26 19:42:20 +0530

    card size fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import frog_img from './Frog_on_palm_frond.jpg';
 export default function Card() {
   return (
     <div
-      className="relative inline-block h-80 w-52 m-4 overflow-hidden shadow-md ease-in-out hover:-translate-y-2 hover:shadow-lg transition-all"
+      className="inline-block h-60 w-40 m-4 overflow-hidden shadow-md ease-in-out hover:-translate-y-2 hover:shadow-lg transition-all"
       style={{
         backgroundImage: `url(${frog_img})`,
         backgroundSize: 'cover',
